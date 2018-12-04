0b8defa81a747b07ce0135be3acfb55d6f09a667
sun_peng_lc@inspur.com <sun_peng_lc@inspur.com>
2018-12-04 17:28:35 +0800

    update

---
+++ flexible_printing/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import flexibleprinting
+from . import res_config_settings
+from . import flexibleprinting_inquiry
+from . import flexibleprinting_workcenter
+from . import flexibleprinting_mo_structure
+from . import flexibleprinting_mrp_workorder
+from . import flexibleprinting_product
+from . import flexibleprinting_product_printing
---
+++ flexible_printing/models/flexibleprinting.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class flexible_printing(models.Model):
+    _name = 'flex_print.flexible_printing'  
+    _description = "Flexible printing default model"
+  
+        
+
